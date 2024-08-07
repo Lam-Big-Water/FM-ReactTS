1ada0fc43ba3f9686c33d418d4d367c051d4693a
Sam Lam <xlin67948gmail.com>
2024-08-07 10:39:55 +0800

    next is Challenge - 3

---
+++ src/Challenge-02.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, useState } from "react";
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+    const [inputV, setInputV] = useState(0);
+
+    const addDouble = () => setCount(count * 2);
+
+    console.log(count);
+    console.log(inputV);
+
+    const handleInput = (event: ChangeEvent<HTMLInputElement>) => setInputV(Number(event.target.value));
+    const handleSubmit = () => {
+        event?.preventDefault();
+        setCount(inputV);
+    }
+    return (
+        <div>
+            <header>{count}</header>
+
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={addDouble}>Double</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+            <p>Set Count</p>
+            <input type="number" onChange={handleInput}/>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Counter;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 // import Challenge from './Challenge.tsx';
+// import Challenge from './Challenge-02.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
